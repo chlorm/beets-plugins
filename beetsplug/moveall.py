e1e9b58ab8a4e00261781a922552ce1f22c190f7
Cody W. Opel <codyopel@gmail.com>
2016-08-28 18:29:38 -0400

    Initial commit

--- beetsplug/moveall.py
+++ beetsplug/moveall.py
@@ -1,17 +1,41 @@
-import os.path
-import os
-import shutil
-import logging
+# Copyright (c) 2013 Dale Sedivec
+# Copyright (c) 2016 Cody Opel <codyopel@gmail.com>
 
-from beets import plugins, library, util
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
 
-log = logging.getLogger("beets")
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
 
+from beets import library, util
+from beets.plugins import BeetsPlugin
+from logging import getLogger
+from os import listdir, path
+from shutil import move, Error
 
-class MoveAllPlugin (plugins.BeetsPlugin):
+log = getLogger('beets')
+
+class MoveAllArtifactsPlugin (BeetsPlugin):
+    """moveallartifacts plugin for beets"""
+
     def __init__(self):
-        super(MoveAllPlugin, self).__init__()
+        super(MoveAllArtifactsPlugin, self).__init__()
+        self.config.add({
+            'exclude_files': [ ],
+            'exclude_exts': [ ],
+        })
         self.register_listener('item_moved', handle_item_moved)
         self.register_listener('cli_exit', handle_cli_exit)
 
@@ -22,36 +46,44 @@ directories_moved = {}
 
 def handle_item_moved(source, destination, **_kwargs):
     global directories_moved
-    src_dir = os.path.dirname(source)
+
+    src_dir = path.dirname(source)
     existing_dst_dir = directories_moved.get(src_dir)
+
     if existing_dst_dir is MULTIPLE_DESTS:
         return
-    dst_dir = os.path.dirname(destination)
+
+    dst_dir = path.dirname(destination)
+
     if existing_dst_dir:
-        if not os.path.samefile(dst_dir, existing_dst_dir):
+        if not path.samefile(dst_dir, existing_dst_dir):
             directories_moved[src_dir] = MULTIPLE_DESTS
-    elif not os.path.samefile(src_dir, dst_dir):
+    elif not path.samefile(src_dir, dst_dir):
         directories_moved[src_dir] = dst_dir
 
 
 def handle_cli_exit(lib, **_kwargs):
     for src_dir, dst_dir in directories_moved.iteritems():
         if dst_dir is MULTIPLE_DESTS:
-            print ("moves out of %s have multiple dests, will not moveall" %
-                   (src_dir,))
+            print("moves out of %s have multiple dests, will not moveall" %
+                  (src_dir,))
             continue
+
         remaining_items = lib.items(library.PathQuery('path', src_dir))
+
         if next(iter(remaining_items), None):
-            print "some Beets items left in %s, will not moveall" % (src_dir,)
-        elif os.path.exists(src_dir):
-            print "moving all leftovers in %s to %s" % (src_dir, dst_dir)
-            for dirent in os.listdir(src_dir):
-                dirent_path = os.path.join(src_dir, dirent)
+            print("some Beets items left in %s, will not moveall" % (src_dir,))
+        elif path.exists(src_dir):
+            print("moving all leftovers in %s to %s" % (src_dir, dst_dir))
+            for dirent in listdir(src_dir):
+                dirent_path = path.join(src_dir, dirent)
+
                 try:
-                    shutil.move(dirent_path, dst_dir)
-                except shutil.Error, ex:
+                    move(dirent_path, dst_dir)
+                except Error, ex:
                     # Log it but move on.
                     # XXX unicode problem here if path has non-ascii
                     log.critical("failed to move {0} to {1}: {2}", dirent_path,
                                  dst_dir, ex)
+
             util.prune_dirs(src_dir)
--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
-import ez_setup
-ez_setup.use_setuptools()
+#!/usr/bin/env python3
 
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name="dale-beets-plugins",
-    version="0.1",
-    packages=setuptools.find_packages(),
-    install_requires=['beets>=1.2.2,==dev'],
+setup(
+    name='beets-moveall-artifacts',
+    version='0.0.0',
+    description='',
+    author='Cody Opel',
+    author_email='codyopel@gmail.com',
+    url='https://github.com/chlorm/beets-moveall-artifacts/',
+    license='MIT',
+    platforms='ALL',
+    long_description=_read('README.rst'),
+    packages=[
+      'moveall',
+    ],
+    install_requires=[
+      'beets>=1.2.19',
+    ],
+    classifiers=[
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
